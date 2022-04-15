2a49ceeb52917a99bf25f9baf1f07e47ed9e178c
Muhkammadaziz <aziz.nigmatjonov7@gmail.com>
2022-04-15 20:59:36 +0500

    Added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,11 @@ window.onload = function() {
   const el = document.getElementById('overlay');
   el.style.display = 'none';
 };
+const currentLocation = location.href;
+const navItem = document.querySelectorAll('.nav-link');
+const navItemLength = navItem.length;
+for (let i = 0; i < navItemLength; i++) {
+  if (navItem[i].href === currentLocation) {
+    navItem[i].classList.add('active');
+  }
+}
